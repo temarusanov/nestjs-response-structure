8aed18804d89ed2c01cb5feb94ab118619bb1587
Kirill Kravchenko <clearworkercorp@gmail.com>
2021-12-15 12:25:41 +0800

    feat: support several nullability options in graphql array response type wrapper

--- lib/types/responses-payload.type.ts
+++ lib/types/responses-payload.type.ts
@@ -1,15 +1,16 @@
 import { Type } from '@nestjs/common'
-import { Field, ObjectType } from '@nestjs/graphql'
+import { Field, NullableList, ObjectType } from '@nestjs/graphql'
 import { ResponseError } from '../interfaces/response-error.interface'
 import { ResponsesPayload } from '../interfaces/responses-payload.interface'
 import { ResponseErrorType } from './response-error.type'
 
 export const ResponsesPayloadType = <T>(
     classRef: Type<T>,
+    nullableList: true | NullableList = true,
 ): Type<ResponsesPayload<T>> => {
     @ObjectType('ResponsesPayload', { isAbstract: true })
     class ResponsesPayloadType implements ResponsesPayload<T> {
-        @Field(() => [classRef], { nullable: true })
+        @Field(() => [classRef], { nullable: nullableList })
         data?: T[]
 
         @Field(() => ResponseErrorType, { nullable: true })
