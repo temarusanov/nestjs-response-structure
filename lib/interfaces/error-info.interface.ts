06c6b80512709e24cc2c2d854623c8125150493f
Kirill Kravchenko <clearworkercorp@gmail.com>
2021-12-15 12:24:21 +0800

    chore: add error info interface

---
+++ lib/interfaces/error-info.interface.ts
@@ -0,0 +1,4 @@
+export interface ErrorInfo {
+    message: string
+    description: string
+}
--- lib/interfaces/index.ts
+++ lib/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from './response-error.interface'
 export * from './response-payload.interface'
 export * from './response-filter-config.interface'
+export * from './error-info.interface'
