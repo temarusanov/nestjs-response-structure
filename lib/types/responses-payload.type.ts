88dd37b16836139f11b6a7c180199b54ed41f9d0
Kirill Kravchenko <clearworkercorp@gmail.com>
2021-12-19 12:39:05 +0800

    chore: add NullableListOptions type to use in 'ResponsesPayloadType' constructor

--- lib/types/responses-payload.type.ts
+++ lib/types/responses-payload.type.ts
@@ -4,18 +4,22 @@ import { ResponseError } from '../interfaces/response-error.interface'
 import { ResponsesPayload } from '../interfaces/responses-payload.interface'
 import { ResponseErrorType } from './response-error.type'
 
+export type NullableListOptions = { nullable: true | 'itemsAndList' }
+
 /**
  *
  * @param classRef Initial class with `@ObjectType` decorator
  * @param options nullability options, returning `[Type]!` or `[Type!]!` is disallowed due to nature of response structure -
  * `data` field will always be (`GraphQL`-wise) null when error is thrown
  *
- * However you can provide `true` (default) to generate `[Type!]` definition
+ * You can provide `true` (default) to generate `[Type!]` definition
  * or `'itemsAndList'` to generate `[Type]` definition in resulting `.graphql` schema file
+ *
+ * @see NullableListOptions
  */
 export const ResponsesPayloadType = <T>(
     classRef: Type<T>,
-    options: { nullable: true | 'itemsAndList' } = { nullable: true },
+    options: NullableListOptions = { nullable: true },
 ): Type<ResponsesPayload<T>> => {
     @ObjectType('ResponsesPayload', { isAbstract: true })
     class ResponsesPayloadType implements ResponsesPayload<T> {
